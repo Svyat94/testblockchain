a8741aa4731420561bf284d02f63e76c11948a57
Svyat94 <vinogradov.s94@yandex.ru>
2023-11-17 16:43:28 +0300

    add Token model

--- api/models.py
+++ api/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
-# Create your models here.
+
+class Token(models.Models):
+    id = models.AutoField(primary_key=True)  #- primary key
+    unique_hash = models.CharField(max_length=64, unique=True, null=True) #- уникальный хэш
+    tx_hash = models.TextField(max_length=255)  #- хэш транзакции создания токена
+    media_url = models.TextField(null=True)  #- урл с произвольным изображением
+    owner = models.TextField(null=True)  #- адрес пользователя в сети Ethereum
